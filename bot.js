5c3302cbdea966747338372d734683775379d432
ShaDes1 <46229960+ShaDes1@users.noreply.github.com>
2018-12-29 01:12:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "ad";
+var prefix = "$";
 
 client.on('message', message => {
 
     if(!message.channel.guild) return;
-if(message.content.startsWith('bc')) {
+if(message.content.startsWith('obc')) {
 if(!message.channel.guild) return message.channel.send('هذا الأمر فقط للسيرفرات').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('للأسف لا تمتلك صلاحية ADMINISTRATOR' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
@@ -44,4 +44,4 @@ msg.delete();
 }
 })
 
-client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+client.login(process.env.BOT_TOKEN);//
