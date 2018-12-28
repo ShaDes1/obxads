bd29b5ac54eee440a9ad44da433124f5397fdae8
ShaDes1 <46229960+ShaDes1@users.noreply.github.com>
2018-12-29 01:19:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "$";
 
@@ -9,12 +9,12 @@ client.on('message', message => {
 
     if(!message.channel.guild) return;
 if(message.content.startsWith('obc')) {
-if(!message.channel.guild) return message.channel.send('هذا الأمر فقط للسيرفرات').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('للأسف لا تمتلك صلاحية ADMINISTRATOR' );
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
 let copy = "====.";
 let request = `Requested By ${message.author.username}`;
-if (!args) return message.reply('يجب عليك كتابة كلمة او جملة لإرسال البرودكاست');message.channel.send(**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \ ${args}`).then(msg => {
+if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
 msg.react('✅')
 .then(() => msg.react('❌'))
 .then(() =>msg.react('✅'))
@@ -24,7 +24,7 @@ let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.
 let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
 let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
 reaction1.on("collect", r => {
-message.channel.send(ballot_box_with_check |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو ).then(m => m.delete(5000));
+message.channel.send(`☑ |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو `).then(m => m.delete(5000));
 message.guild.members.forEach(m => {
 var bc = new
 Discord.RichEmbed()
@@ -40,11 +40,12 @@ msg.delete();
 })
 })
 reaction2.on("collect", r => {
-message.channel.send(Broadcast Canceled.`).then(m => m.delete(5000));
+message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
 msg.delete();
 })
 })
 }
 })
 
-client.login(process.env.BOT_TOKEN);//
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
