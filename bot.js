b8094410ab78772d5e37ff6c18bc7158f9bde2de
ShaDes1 <46229960+ShaDes1@users.noreply.github.com>
2019-02-02 16:48:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
  const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "$";
+var prefix = "a";
 
 
 
@@ -8,7 +8,7 @@ var prefix = "$";
 client.on('message', message => {
 
     if(!message.channel.guild) return;
-if(message.content.startsWith('abc')) {
+if(message.content.startsWith('bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
